f04e786b613bf3c8afe754a4c6f188b087338681
Avishek Barua <avishekbarua02@gmail.com>
2022-11-06 14:37:25 +0600

    ...

---
+++ script.js
@@ -0,0 +1,88 @@
+const daySelect = document.getElementById('day');
+const monthSelect = document.getElementById('month');
+const yearSelect = document.getElementById('year');
+
+const months = [
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+];
+
+(function populateMonths() {
+    for (let i = 0; i < months.length; i++) {
+        const option = document.createElement('option');
+        option.textContent = months[i];
+        monthSelect.appendChild(option);
+    }
+    monthSelect.value = 'January'
+})();
+
+let previousDay;
+
+function populateDays(month) {
+    while (daySelect.firstChild) {
+        daySelect.removeChild(daySelect.firstChild);
+    }
+
+    let dayNum;
+    let year = yearSelect.value;
+
+    if (month === 'January' || month === 'March' || month === 'May' || month === 'July' || month === 'August' || month === 'October' || month === 'December') {
+        dayNum = 31
+    } else if (month === 'April' || month === 'June' || month === 'September' || month === 'November') {
+        dayNum = 30
+    } else {
+        if (new Date(year, 1, 29).getMonth() === 1) {
+            dayNum = 29;
+        } else {
+            dayNum = 28;
+        }
+    }
+    for (let i = 1; i <= dayNum; i++) {
+        const option = document.createElement('option');
+        option.textContent = i;
+        daySelect.appendChild(option);
+    }
+    if (previousDay) {
+        daySelect.value = previousDay;
+        if (daySelect.value === '') {
+            daySelect.value = previousDay - 1;
+        }
+        if (daySelect.value === '') {
+            daySelect.value = previousDay - 2;
+        }
+        if (daySelect.value === '') {
+            daySelect.value = previousDay - 3;
+        }
+    }
+}
+
+function populateYears() {
+    let year = new Date().getFullYear();
+    for (let i = 0; i < 101; i++) {
+        const option = document.createElement('option');
+        option.textContent = year - i;
+        yearSelect.appendChild(option);
+
+    }
+}
+
+populateDays(monthSelect.value);
+populateYears();
+
+yearSelect.onchange = function () {
+    populateDays(monthSelect.value);
+}
+monthSelect.onchange = function () {
+    populateDays(monthSelect.value);
+}
+
+daySelect.onchange = function () {
+    previousDay = daySelect.value;
+}
+
+const selectDate = document.getElementById('calender');
+
+selectDate.onchange = function(){
+    yearSelect.value = (selectDate.value).split('-')[0];
+    monthSelect.value = months[((selectDate.value).split('-')[1])-1];
+    daySelect.value = (selectDate.value).split('-')[2];
+}
